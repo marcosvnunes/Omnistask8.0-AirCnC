bf614e7e0e0a8a8db4bf15c0ea379a7bb576269f
Marcos <marcosnunescomercial@gmail.com>
2020-01-13 15:10:01 -0300

    upload

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.Server(app);
 const io = socketio(server)
 
-mongoose.connect('mongodb+srv://tickedev:popgg2w8z7gg@cluster0-9vtgm.mongodb.net/semana09?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://<username>:<password>@cluster0-9vtgm.mongodb.net/semana09?retryWrites=true&w=majority',
 {   
     useNewUrlParser:true,
     useUnifiedTopology:true,
